7f4c3c09baf9967fe823c4e731f9186c624772de
Mike Petersen <mike@odania-it.de>
2016-02-25 22:51:04 +0100

    Updated docker compose file and static fixtures

---
+++ bin/update_odania_gem.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+ODANIA_GEM_FOLDER_NAME=$(cat config/config.yml | grep odania-gem | awk '{ print $1 }' | tr -d ':')
+VERSION=$(cat git/odania-gem/lib/odania/version.rb | grep VERSION | awk '{ print $3 }' | tr -d "'")
+ODANIA_GEM_FOLDER=git/$ODANIA_GEM_FOLDER_NAME
+
+echo "Odania Version: ${VERSION} (${ODANIA_GEM_FOLDER})"
+cd $ODANIA_GEM_FOLDER
+rake build
+cd ..
+
+echo "Core & HAProxy Internal"
+cp $ODANIA_GEM_FOLDER_NAME/pkg/odania-${VERSION}.gem odania-core
+docker exec -ti odaniadevelop_core_1 gem install odania-${VERSION}.gem
+docker exec -ti odaniadevelop_core_1 bundle install
+
+echo "Varnish Public"
+cp $ODANIA_GEM_FOLDER_NAME/pkg/odania-${VERSION}.gem odania-varnish
+docker exec -ti odaniadevelop_varnish_1 gem install odania-${VERSION}.gem
+docker exec -ti odaniadevelop_varnish_1 bundle install
+
+echo "Odania Static"
+cp $ODANIA_GEM_FOLDER_NAME/pkg/odania-${VERSION}.gem odania-static
+docker exec -ti odaniadevelop_odaniastatic_1 gem install odania-${VERSION}.gem
+docker exec -ti odaniadevelop_odaniastatic_1 bundle install
---
+++ bin/update_services.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+echo "Odania Static"
+docker exec -ti odaniadevelop_odaniastatic_1 rake web:generate
+
+echo "Core & Varnish Internal"
+docker exec -ti odaniadevelop_core_1 rake odania:global:generate_config
+docker exec -ti odaniadevelop_core_1 rake odania:haproxy:internal
+docker exec -ti odaniadevelop_core_1 rake odania:register
+
+echo "Varnish Public"
+docker exec -ti odaniadevelop_varnish_1 rake varnish:generate
